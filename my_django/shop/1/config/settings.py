be81bc0a6b3b53bc9fea96e5af2127850a643e51
Kalys <jsvsjsvshsksb@gmail.com>
2022-11-30 15:07:42 +0600

    first commi

--- my_django/shop/1/config/settings.py
+++ my_django/shop/1/config/settings.py
@@ -84,9 +84,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
+        'NAME': 'shop',
+        'USER': 'kalys',
+        'PASSWORD': '1',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
